eb06cf81e436f5db93fd08bbb168901c152c3480
WhyK <windchimeyk@gmail.com>
2021-03-29 02:13:51 +0900

    feat: add json

--- api/index.go
+++ api/index.go
@@ -1,10 +1,25 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
+type Todo struct {
+	Name     string    `json:"name"`
+	Computed bool      `json:"computed"`
+	Due      time.Time `json:"due"`
+}
+
+type Todos []Todo
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	todos := Todos{
+		Todo{Name: "Write presentation"},
+		Todo{Name: "Host meetup"},
+	}
+	json.NewEncoder(w).Encode(todos)
+	fmt.Println("Encode JSON")
 }
