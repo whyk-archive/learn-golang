5683f9f504dfa22cb4ddc28aee570b1fbfc379c4
WhyK <windchimeyk@gmail.com>
2021-03-31 01:06:20 +0900

    feat: add dotenv

--- api/index.go
+++ api/index.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/caarlos0/env/v6"
 )
 
 type Todo struct {
@@ -15,7 +17,21 @@ type Todo struct {
 
 type Todos []Todo
 
+type Config struct {
+	APIKey string `env:"API_KEY"`
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
+	var conf Config
+	if err := env.Parse(&conf); err != nil {
+		panic(err)
+	}
+
+	authorization := r.Header.Get("API_KEY")
+	if authorization == "" || authorization != conf.APIKey {
+		return
+	}
+
 	todos := Todos{
 		Todo{Name: "Write presentation"},
 		Todo{Name: "Host meetup"},
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module learn-golang
 
 go 1.15
+
+require (
+	github.com/caarlos0/env/v6 v6.5.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+)
